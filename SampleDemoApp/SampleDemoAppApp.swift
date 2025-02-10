cf3def60b421ddb0445ef2490607bc9587fd3382
Rayhan <shadmaumder@gmail.com>
2025-02-10 19:15:43 +0600

    Initial Commit

---
+++ SampleDemoApp/SampleDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDemoAppApp.swift
+//  SampleDemoApp
+//
+//  Created by Mazumder on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
