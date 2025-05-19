7b226ec8d380778dba8b1e21d0ca70ee1aa36865
Rayhan <shadmaumder@gmail.com>
2025-05-19 06:33:58 +0600

    Updating text to OTA

--- SampleDemoApp/ContentView.swift
+++ SampleDemoApp/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hi, there!")
+            Text("OTA!")
         }
         .padding()
     }
